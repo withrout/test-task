675cd76ce92e9589c21604b3764902a8daa541f5
withrout <withrout@gmail.com>
2022-06-06 16:38:29 +0700

    Добавил логгирование

--- src/main/java/ru/alfabank/service/impl/ExchangeRatesServiceImpl.java
+++ src/main/java/ru/alfabank/service/impl/ExchangeRatesServiceImpl.java
@@ -1,6 +1,8 @@
 package ru.alfabank.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.alfabank.client.ExchangeRatesFeignClient;
 import ru.alfabank.dto.RatesDto;
@@ -21,17 +23,22 @@ public class ExchangeRatesServiceImpl implements ExchangeRatesService {
     private static final String TAKE_RATE_ERROR = "Произошла ошибка при получении курса валюты";
     private static final String TAKE_YESTERDAY_RATE_ERROR = "Произошла ошибка при получении вчерашнего курса валюты";
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExchangeRatesService.class);
+
     private final ExchangeRatesFeignClient feignClient;
 
     @Override
     public List<String> takeAllCurrencies() {
         try {
-            return feignClient
+            var currenciesList = feignClient
                     .takeAllCurrencies()
                     .keySet()
                     .stream()
                     .toList();
+            LOGGER.info("Получен список курсов валют в количестве: {}", currenciesList.size());
+            return currenciesList;
         } catch (ExchangeRateServiceException e) {
+            LOGGER.warn("Произошла ошибка при запросе к API курсов валют!");
             throw new ExchangeRateServiceException(TAKE_ALL_CURRENCIES_ERROR);
         }
     }
@@ -39,12 +46,13 @@ public class ExchangeRatesServiceImpl implements ExchangeRatesService {
     @Override
     public RatesDto takeRateByCode(String code) {
         try {
+            var rate = feignClient.takeRateByCode(code).getRates().get(code);
+            LOGGER.info("Запрошен курс валюты, код: {}", code);
             return RatesDto.builder()
-                    .rate(feignClient.takeRateByCode(code)
-                            .getRates()
-                            .get(code))
+                    .rate(rate)
                     .build();
         } catch (ExchangeRateServiceException e) {
+            LOGGER.warn("Произошла ошибка при запросе к API курсов валют!");
             throw new ExchangeRateServiceException(TAKE_RATE_ERROR);
         }
     }
@@ -55,12 +63,13 @@ public class ExchangeRatesServiceImpl implements ExchangeRatesService {
             var dateOfYesterday = (LocalDate.ofInstant(Instant.now(), ZoneId.of("UTC"))
                     .minusDays(1))
                     .format(DateTimeFormatter.ISO_DATE);
+            var rate = feignClient.takeRateByDateAndCode(dateOfYesterday, code).getRates().get(code);
+            LOGGER.info("Запрошен вчерашний курс валюты, код: {}", code);
             return RatesDto.builder()
-                    .rate(feignClient.takeRateByDateAndCode(dateOfYesterday, code)
-                            .getRates()
-                            .get(code))
+                    .rate(rate)
                     .build();
         } catch (ExchangeRateServiceException e) {
+            LOGGER.warn("Произошла ошибка при запросе к API курсов валют!");
             throw new ExchangeRateServiceException(TAKE_YESTERDAY_RATE_ERROR);
         }
     }
--- src/main/java/ru/alfabank/service/impl/GiphyServiceImpl.java
+++ src/main/java/ru/alfabank/service/impl/GiphyServiceImpl.java
@@ -1,6 +1,8 @@
 package ru.alfabank.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.alfabank.client.GiphyFeignClient;
@@ -17,6 +19,7 @@ import java.util.Random;
 public class GiphyServiceImpl implements GiphyService {
 
     private static final String GIPHY_ERROR = "Произошла ошибка при взаимодействии с сервисом гифов";
+    private static final Logger LOGGER = LoggerFactory.getLogger(GiphyService.class);
 
     @Value("#{${giphy.q}}")
     private Map<Integer, String> keys;
@@ -40,6 +43,7 @@ public class GiphyServiceImpl implements GiphyService {
         var toDayRate = ratesService.takeRateByCode(code).getRate();
         var yesterdayRate = ratesService.takeYesterdayRateByCode(code).getRate();
         var gif = feignClient.getSearchGifResult(keys.get(Double.compare(toDayRate, yesterdayRate)));
+        LOGGER.info("Получен GIF с названием: {}", gif.getTitle());
         return GifDto.builder()
                 .title(gif.getTitle() != null ? gif.getTitle() : "")
                 .url(gif.getUrls().get(new Random().nextInt(Integer.parseInt(limit))))
